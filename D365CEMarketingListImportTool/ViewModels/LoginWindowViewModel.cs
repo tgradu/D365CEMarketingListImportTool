86a1568ab50d4abf8102249e62fc8ab66dc80ab3
Radu Chiribelea <raduc@outlook.com>
2019-11-02 23:13:12 +0100

    added new files

--- D365CEMarketingListImportTool/ViewModels/LoginWindowViewModel.cs
+++ D365CEMarketingListImportTool/ViewModels/LoginWindowViewModel.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using D365CEMarketingListImportTool.MVVMFramework;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace D365CEMarketingListImportTool.ViewModels
 {
-    public class LoginWindowViewModel
+    public class LoginWindowViewModel : INotifyPropertyChanged
     {
+        [field: NonSerialized]
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #region Fields
+        #endregion Fields
+
+        #region Properties
+
+        public string UserName { get; set; }
+        public string D365CEUrl { get; set; }
+        public SecureString Password { get; set; }
+
+        #endregion Properties
+
+        #region Constructors
+        public LoginWindowViewModel()
+        {
+        }
+
+        #endregion Constructors
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommandAsync(BuildCrmConnection);
+            }
+        }
+
+
+        #endregion Commands
+
+        #region Methods
+        private async Task BuildCrmConnection(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion Methods
     }
 }
