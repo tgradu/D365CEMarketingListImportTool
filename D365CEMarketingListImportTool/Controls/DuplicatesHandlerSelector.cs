0ce4ac0fee77683a81d91353e90382c0231167db
Radu Chiribelea <raduc@outlook.com>
2019-11-13 23:33:04 +0100

    stuff

---
+++ D365CEMarketingListImportTool/Controls/DuplicatesHandlerSelector.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D365CEMarketingListImportTool.Controls
+{
+    public class DuplicatesHandlerSelector
+    {
+        https://stackoverflow.com/questions/29176922/observabledictionary-binding-to-combobox-display-valuemvvm
+    }
+}
